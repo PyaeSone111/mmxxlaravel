d22537f1218da088c246a820afcbe1c4efe3c56a
Pyae Sone <payesone@stickie.tech>
2026-02-15 00:38:26 +0700

    allow cors origin

--- config/cors.php
+++ config/cors.php
@@ -24,10 +24,16 @@ return [
         'http://127.0.0.1:3000',
         'http://localhost:3001',
         'http://127.0.0.1:3001',
+        'https://mmxx-fe4a7.firebaseapp.com',
+        'https://mmxx-fe4a7.web.app',
+        'https://apis.mm18plus.com',
+        'https://mm18plus.com',
+        'https://www.mm18plus.com'
     ],
 
     'allowed_origins_patterns' => [
         '#^https?://(localhost|127\.0\.0\.1)(:\d+)?$#',
+        '#^https://[a-z0-9-]+\.(firebaseapp\.com|web\.app)$#',
     ],
 
     'allowed_headers' => ['*'],
